3b42f65b5d18ef96be56deb4d70a4e869d697fdf
eSaadster <64090165+eSaadster@users.noreply.github.com>
2025-06-05 03:15:57 -0600

    Add shared logger and integrate into gateway

--- nimbusvault/gateway/index.js
+++ nimbusvault/gateway/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const logger = require('../shared/logger');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  logger.info(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello from gateway');
 });
 
 app.listen(PORT, () => {
-  console.log(`Gateway running on port ${PORT}`);
+  logger.info(`Gateway running on port ${PORT}`);
 });
